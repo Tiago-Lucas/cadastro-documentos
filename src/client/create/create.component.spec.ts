c107de25fa141a853eeab78a52bb2de9153b5251
Tiago-Lucas <lucastiago45@gmail.com>
2021-05-28 14:24:30 -0300

    atualizando components

--- src/client/create/create.component.spec.ts
+++ src/client/create/create.component.spec.ts
@@ -22,4 +22,8 @@ describe('CreateComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
